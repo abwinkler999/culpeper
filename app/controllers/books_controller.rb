3b456002644aedc953adfb85e495516645b03c54
Benjamin Winkler <ben@hardboiledgeek.com>
2012-03-18 00:54:30 -0400

    Edit to catalog entry view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,25 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.find(:all, :order => 'title')
   end
   
   def show
     @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params[:book])
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 end
