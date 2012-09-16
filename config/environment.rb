28179d9cfc6ecb8b58c87228a196e0990121e23f
Benjamin Winkler <ben@hardboiledgeek.com>
2012-09-16 18:47:06 -0400

    Minor

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+USER = "Gahanna Herb Society"
+
 # Initialize the rails application
 Culpeper::Application.initialize!
