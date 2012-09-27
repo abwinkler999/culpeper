46cc19dc3c1cc540aa8184de24d687441410e705
Benjamin Winkler <ben@hardboiledgeek.com>
2012-09-27 16:35:11 -0400

    HAML FTW

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120916181427) do
+ActiveRecord::Schema.define(:version => 20120927190357) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120916181427) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "genre"
+    t.string   "isbn"
   end
 
 end
